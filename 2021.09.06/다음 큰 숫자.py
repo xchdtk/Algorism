03bd01a381c7773cdabbd478f9dd83aa65ea0de5
kimjinsoo <1004kjs_@naver.com>
2021-09-07 23:59:57 +0900

    알고리즘 공부 완료

--- 2021.09.06/다음 큰 숫자.py
+++ 2021.09.06/다음 큰 숫자.py
@@ -1,16 +1,32 @@
 def next_max_number(number): 
-    pass
-    max_number = 1000001
-    for n in range(number, max_number):
-        new_number = n
+    len_one = 0
+    max_number = 79
+    for n in range(1):
+        new_number = 78
         stack = []
+        count= 0
         while new_number != 1 or new_number != 0:
+            count += 1
             share     = new_number // 2
             remainder = new_number % 2
+
+            print(new_number)
+            print(share)
+            print(remainder)
+            print(stack)len_one
 
             new_number = share
             if remainder:
                 stack.append(remainder)
+
+            if count == 10:
+                return
+        if new_number:
+            stack.append(new_number)
+        return len_one
+
+number = 78
+print(next_max_number(number))
 
 
             
